d9684fb83c953774af69a702ae165539640ec027
JUsmaa <julian.usma09@gmail.com>
2024-08-13 10:06:45 -0500

    se anadio variables logicas con su explicacion

---
+++ script.js
@@ -0,0 +1,69 @@
+/* Datos de entrada 
+Console.log */
+
+/* Datos de salida
+prompt --> como el sccanner */
+
+
+/* Datos de proceso
+operaciones aritmeticas y logicas, suma, resta, multiplicacion,
+divicion, comparacion, asignacion, decremento, etc.
+
+Aritmeticos:
+suma: valor1 + valor2
+resta: valor1 - valor2
+multiplicacion :valor1 * valor2
+divicion: valor1 / valor2
+Logicos(generan un resultado booleano: true o false.): 
+<   -> Mayor que (excluyente)
+>   -> Menor que (excluyente)
+<=  -> Menor o igual (incluyente)
+>=  -> Mayor o igual (incluyente)
+==  -> Comparasion simple
+=== -> comparacion Estricta
+!=  -> Negacion simple
+!== -> Negacion estricta
+*/
+
+
+/* prompt("ingrese su nombre: ")
+
+var NombreCompleto = "julian 777"
+console.log (NombreCompleto) */
+
+
+/* var numeroUno = parseInt(prompt ('ingrese un numero: '))
+var numeroDos = Number (prompt ('ingrese otro numero: '))
+console.log(numeroUno + numeroDos); */
+
+/* parsear para combertir una variable de script a variable numerica */
+
+/* var NombreCompleto = 'Julian Usma'
+var NombreCompleto = 'Perez'
+console.log (NombreCompleto) */
+
+/* Las variables de tipo var permiten la sobreescritura de variables y de contenido */
+
+/* let saldoCuenta = '20000000'
+let saldoCuenta = '2000'
+console.log (saldoCuenta) */
+
+/* las variables tipo let no permiten sobreescritura de variables pero si de contenido */
+
+/* const saldoCuenta = '20000000'
+saldoCuenta = '2000'
+console.log (saldoCuenta) */
+
+/* Las variables tipo const no permiten la sobreescritura de variables ni de contenido */
+
+/* las variables de tipo let y const son la mas utilizadas */
+
+/* exclyentes */
+console.log(3>3)
+console.log(3<7)
+/* incluyentes */
+console.log(3>=3)
+console.log(3<=7)
+/* comparaciones simples y extrictas */
+console.log(3 == 3)
+console.log(3 === '3')
